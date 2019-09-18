4f7a736af6b68235aedebd6b1d6376b6da7dc98d
mcv1993 <mcv1993@gmail.com>
2019-09-18 21:17:51 +0000

    add icon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,11 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_path
+  end
+
   private
 
   def user_params
