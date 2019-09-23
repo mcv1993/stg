7e4d6420edb76ff5a17db5726420c6d9b924fae4
mcv1993 <mcv1993@gmail.com>
2019-09-23 02:05:25 +0000

    admin create user thru email - added confirmable properties and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_234119) do
+ActiveRecord::Schema.define(version: 2019_09_23_015646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 2019_09_19_234119) do
     t.boolean "admin", default: false
     t.boolean "superadmin", default: false
     t.string "api_key"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
